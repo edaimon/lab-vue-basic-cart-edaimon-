c51abf317ea79d2ad3bc550cd67560a0555a6ac3
edaimon <eledaimon@yahoo.com>
2022-12-24 01:35:54 +0100

    Final update

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,57 @@
-<!-- <style>
-.navbar {
-  text-decoration: none;
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-}
-
-</style> -->
-
-
 <script>
-import ITEMS from './items.json';
-export default {
-  data() {
-    return {
-      items: ITEMS,
-      // Iteration 2.1
-      sumaSimple: 2+2,
-
-    };
-    
-  },
-
-  methods: {
-    isItEmptyCart(items) {
-      return this.items.length >0 ? 'Yes' : 'No';
-
+import { computed } from "@vue/reactivity";
+import ITEMS from "./items.json";
+  export default {
+    data() {
+      return {
+        items: ITEMS,
+        logo: "Ironhack_logo.svg",
+        user: "user.png",
+        shoppingCart: "cart.png",
+      }
+    },
+    computed: {
+      cartLimit(){
+        return this.items.length >= 5;
+      },
+      numberOfItems(){
+        return this.items.length;
+      }
+    }
   }
-
-
-  }
-}
-
-
 </script>
 
 <template>
-  <header>
-    <!-- Iteration 1 -->
-    <navbar class="navbar">
-      
-      <p>// Iteration 1</p>
-      <ul>
-        <li>Home</li>
-        <li>Products</li>
-        <li>Services</li>
-        <li>Contact</li>
-      </ul>
-    </navbar>
-  </header>
-  <hr/>
-
+  <nav class="flex flex-row justify-between mx-8 my-4">
+    <img :src="logo" alt="Ironhack logo">
+    <ul class="flex ">
+      <li class="mx-20 text-lg text-violet-900 hover:font-bold"><a href="#">Home</a></li>
+      <li class="mx-20 text-lg text-violet-900 hover:font-bold"><a href="#">Services</a></li>
+      <li class="mx-20 text-lg text-violet-900 hover:font-bold"><a href="#">Shop</a></li>
+    </ul>
+    <div class="flex">
+      <img :src="user" alt="user profile" class="h-6 mx-8">
+      <img :src="shoppingCart" alt="cart" class="h-6">
+      <div class="mx-4 font-bold">{{numberOfItems}}</div>
+    </div>
+  </nav>
+  <table class="w-3/4 my-20 bg-slate-200 mx-auto">
+    <tr v-for="item of items" key="item.uuid" class="flex justify-between">
+      <td><img :src="item.image" :alt="item.name" class="w-16"></td>
+      <td class="text-lg font-bold mx-4">{{item.name}}</td>
+      <td>{{item.price}}</td>
+      <td><input type="number" v-model="item.quantity" placeholder="0" class="border-2 mx-16"></td>
+      <td>{{(item.quantity * item.price)}}</td>
+    </tr>
+  </table>
   <div>
-    <p>// Iteration 2.1</p>
-    <p>Simple addition from variable: </p> {{ sumaSimple }}
-    <hr/>
+    <button v-if="cartLimit" class="float-right m-12 border-2 border-violet-500 p-2">Confirm purchase</button>
+    <p v-else>Add at least 5 products for free shipping!</p>
   </div>
-  <div>
-    <p>// Iteration 2.2</p>
-    <p>Print a string reading reactive variables:</p>
-    <p>Are they products in the cart? {{ isItEmptyCart() }}</p>
-    
-  </div>
-  <hr/>
-  <div>
-    <p>// Iteration 3</p>
-    <div v-if="(items.length < 6)">Si compras más de 6 productos, envío gratis</div> 
-
-  </div>
-  <br/>
-  <hr/>
-  <p>// Iteration 4</p>
-  <div v-for="(item, index) of items" :key="item.uuid">
-    <div>Uuid: {{ index }} | Project Name: {{ item.name }}</div>
-    
-  </div>
-  
-  <main>
-   
-  </main>
-<hr/><br/><br/>
-  <footer>@1985 Spectrum ZX corp</footer>
 </template>
+
+<style scoped>
+
+</style>
+
 
